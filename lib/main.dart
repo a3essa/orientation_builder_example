9e28bad08301630cabe7bf9493ba68ecf7c36432
a3essa <ahmads3essa@gmail.com>
2022-05-24 21:26:48 +0300

    Add assigment 2 project

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ Future main() async {
 }
 
 class MyApp extends StatelessWidget {
-  static final String title = 'Orientation Builder';
+  static final String title = 'Ahmed Issa - Assignment-2 ';
 
   @override
   Widget build(BuildContext context) => MaterialApp(
@@ -38,7 +38,7 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> with WidgetsBindingObserver {
   @override
   Widget build(BuildContext context) => Scaffold(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text(widget.title),
           actions: [
@@ -49,7 +49,6 @@ class _MainPageState extends State<MainPage> with WidgetsBindingObserver {
           ],
         ),
         body: Container(
-          padding: EdgeInsets.all(32),
           child: OrientationBuilder(
             builder: (context, orientation) =>
                 orientation == Orientation.portrait
@@ -57,8 +56,44 @@ class _MainPageState extends State<MainPage> with WidgetsBindingObserver {
                     : buildLandscape(),
           ),
         ),
+        drawer: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Drawer Header'),
+              ),
+              ListTile(
+                title: const Text('Item 1'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Item 2'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.rotate_left),
+          backgroundColor: Colors.blue,
           onPressed: () {
             final isPortrait =
                 MediaQuery.of(context).orientation == Orientation.portrait;
@@ -87,7 +122,6 @@ class _MainPageState extends State<MainPage> with WidgetsBindingObserver {
 
   Widget buildPortrait() => ListView(
         children: [
-          buildImage(),
           const SizedBox(height: 16),
           buildText(),
         ],
@@ -95,19 +129,15 @@ class _MainPageState extends State<MainPage> with WidgetsBindingObserver {
 
   Widget buildLandscape() => Row(
         children: [
-          buildImage(),
           const SizedBox(width: 16),
           Expanded(child: SingleChildScrollView(child: buildText())),
         ],
       );
 
-  Widget buildImage() => Image.network(
-      'https://images.unsplash.com/photo-1585129819171-80b02d4c85b0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80');
-
   Widget buildText() => Column(
         children: [
           Text(
-            'Hair Styling',
+            'Student n. 120191913',
             style: TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 32,
@@ -116,7 +146,7 @@ class _MainPageState extends State<MainPage> with WidgetsBindingObserver {
           ),
           const SizedBox(height: 16),
           Text(
-            '''The oldest known depiction of hair styling is hair braiding which dates back about 30,000 years. In history, women's hair was often elaborately and carefully dressed in special ways. From the time of the Roman Empire[citation needed] until the Middle Ages, most women grew their hair as long as it would naturally grow. Between the late 15th century and the 16th century, a very high hairline on the forehead was considered attractive. Around the same time period, European men often wore their hair cropped no longer than shoulder-length. In the early 17th century, male hairstyles grew longer, with waves or curls being considered desirable.''',
+            '''You can change portrait and landscap by click on floating action button.''',
             style: TextStyle(fontSize: 20, color: Colors.white),
           ),
         ],
